18a4da8f5019788bd5fc2f03bc256a12127dd910
Adenilson Soares <adenilson_denis8@hotmail.com>
2025-10-12 13:50:46 -0300

    Evoluções

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { Container } from '../../components/container';
 import { Input } from '../../components/input';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+
+import { auth } from '../../services/firebaseConnection';
+import { createUserWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import {AuthContext} from '../../contexts/AuthContext';
+
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
@@ -15,13 +21,40 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>
 
 export function Register() {
+  const {handleInfoUser} = useContext(AuthContext);
+  const navigate = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: "onChange"
   })
 
-  function onSubmit(data: FormData) {
-    console.log(data)
+  useEffect(() =>{
+      async function handleLogout(){
+        await signOut(auth)
+      }
+      handleLogout();
+    },[])
+
+  async function onSubmit(data: FormData) {
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+    .then(async (user) =>{
+      await updateProfile(user.user, {
+        displayName: data.name
+      })
+
+      handleInfoUser({
+        name: data.name,
+        email: data.email,
+        uid: user.user.uid
+      })
+      console.log("CADASTRADO COM SUCESSO!");
+      navigate("/dashboard", { replace: true });
+
+    })
+    .catch((error) =>{
+      console.log("ERRO AO CADASTRAR ESSE USUÁRIO");
+      console.log(error);
+    })
   }
 
   return (
@@ -68,7 +101,7 @@ export function Register() {
             />
           </div>
           <button type="submit" className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium">
-            Acessar
+            Cadastrar
           </button>
         </form>
         <Link to="/login">
