97971f4c57bdfbdf42b7498801d1dca8a8777dfb
Adenilson Soares <adenilson_denis8@hotmail.com>
2026-01-02 14:57:10 -0300

    Home - Consultado os registros do banco de dados

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,8 +1,8 @@
-import { ChangeEvent, useState, useContext, use } from "react";
+import { ChangeEvent, useState, useContext } from "react";
 import { Container } from "../../../components/container";
 import { DashboardHeader } from "../../../components/panelheader";
 
-import { FiUpload } from 'react-icons/fi';
+import { FiUpload, FiTrash } from 'react-icons/fi';
 import { useForm } from 'react-hook-form';
 import { Input } from '../../../components/input';
 import { z } from 'zod';
@@ -10,13 +10,15 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { AuthContext } from '../../../contexts/AuthContext';
 import { v4 as uuidV4 } from 'uuid';
 
-import { storage } from "../../../services/firebaseConnection";
+import { storage, db } from "../../../services/firebaseConnection";
 import {
   ref,
   uploadBytes,
   getDownloadURL,
   deleteObject
 } from 'firebase/storage';
+
+import {addDoc, collection} from 'firebase/firestore';
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
@@ -33,12 +35,21 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
+interface ImageItemProps{
+  uid: string;
+  name: string;
+  previewUrl: string;
+  url: string
+}
+
 export function New() {
   const { user } = useContext(AuthContext);
   const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: "onChange"
   })
+
+  const [carImages, setCarImages] = useState<ImageItemProps[]>([])
 
   async function handleFile(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.files && e.target.files[0]) {
@@ -67,14 +78,67 @@ export function New() {
     uploadBytes(uploadRef, image)
       .then((snapshot) => {
         getDownloadURL(snapshot.ref).then((downloadUrl) => {
-          console.log("URL DE ACESSO DA FOTO: ", downloadUrl);
+          const imageItem = {
+            name: uidImage,
+            uid: currentUid,
+            previewUrl: URL.createObjectURL(image),
+            url: downloadUrl,
+          }
+
+          setCarImages((images) => [...images, imageItem])
         })
       })
 
   }
 
   function onSubmit(data: FormData) {
-    console.log(data);
+    if(carImages.length === 0){
+      alert("Envie alguma imagem deste carro!")
+    }
+
+    const carListImages = carImages.map(car => {
+      return{
+        uid: car.uid,
+        name: car.name,
+        url: car.url
+      }
+    })
+
+    addDoc(collection(db, "cars"), {
+      name: data.name,
+      model: data.model,
+      whatsapp: data.whatsapp,
+      city: data.city,
+      year: data.year,
+      km: data.km,
+      price: data.price,
+      description: data.description,
+      created: new Date(),
+      owner: user?.name,
+      uid: user?.uid,
+      images: carListImages,
+    })
+    .then(() => {
+      reset();
+      setCarImages([]);
+      console.log("CADASTRADO COM SUCESSO!");
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("ERRO AO CADASTRAR NO BANCO");
+    })
+  }
+
+  async function handleDeleteImage(item: ImageItemProps){
+    const imagePath = `images/${item.uid}/${item.name}`;
+
+    const imageRef = ref(storage, imagePath);
+    try{
+      await deleteObject(imageRef);
+      setCarImages(carImages.filter((car) => car.url !== item.url))
+    }catch(err){
+      console.log("ERRO AO DELETAR")
+    }
   }
 
   return (
@@ -91,6 +155,19 @@ export function New() {
             <input type="file" accept="image/*" className="opacity-0 cursor-pointer" onChange={handleFile} />
           </div>
         </button>
+
+        {carImages.map(item => (
+          <div key={item.name} className="w-full h-32 flex items-center justify-center relative">
+            <button className="absolute" onClick={() => handleDeleteImage(item)}>
+              <FiTrash size={28} color="#FFF"/>
+            </button>
+            <img
+              src={item.previewUrl}
+              className="rounded-lg w-full h-32 object-cover"
+              alt="Foto do carro"
+            />
+          </div>
+        ))}
       </div>
 
       <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,76 @@
+import { useState, useEffect } from "react";
 import { Container } from "../../components/container";
+import { Link } from 'react-router-dom';
 
-export function Home(){
-  return(
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy
+} from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection';
+
+interface CarsProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
+
+export function Home() {
+  const [cars, setCars] = useState<CarsProps[]>([]);
+  const [loadImages, setLoadImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars")
+      const queryRef = query(carsRef, orderBy("created", "desc"))
+
+      getDocs(queryRef)
+        .then((snapshot) => {
+          let listcars = [] as CarsProps[];
+
+          snapshot.forEach(doc => {
+            listcars.push({
+              id: doc.id,
+              name: doc.data().name,
+              year: doc.data().year,
+              km: doc.data().km,
+              city: doc.data().city,
+              price: doc.data().price,
+              images: doc.data().images,
+              uid: doc.data().uid
+            })
+          })
+
+          console.log(listcars);
+          setCars(listcars);
+
+        })
+    }
+
+    loadCars();
+  }, [])
+
+  function handleImageLoad(id:string){
+    setLoadImages((prevImageLoaded) => [...prevImageLoaded, id])
+  }
+
+  return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
         <input
-          className="w-full border-2 rounded-lg h-9 px-3 outline-none" 
+          className="w-full border-2 rounded-lg h-9 px-3 outline-none"
           placeholder="Digite o nome do carro..."
         />
         <button
@@ -21,24 +86,34 @@ export function Home(){
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2025/202509/20250902/chevrolet-onix-1.0-turbo-flex-plus-premier-automatico-wmimagem01094750417.jpg?s=fill&w=1920&h=1440&q=75"
-            alt="Carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">Chevrolet Onix 1.0</p>
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">Ano 2024/2025 | 25.334 km</span>
-            <strong className="text-black font-medium text-xl">R$ 111.000</strong>
-          </div>
-          <div className="w-full h-px bg-slate-200 my-2"></div>
-          <div className="px-2 pb-2">
-            <span className="text-black">
-              Campo Grande - MS
-            </span>
-          </div>
-        </section>
+        {cars.map(car => (
+          <Link  key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg">
+              <div 
+              className="w-full h-72 rounded-lg bg-slate-200"
+              style={{display: loadImages.includes(car.id) ? "none" : "block"}}
+              ></div>
+              <img
+                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car.images[0].url}
+                alt="Carro"
+                onLoad={() => handleImageLoad(car.id)}
+                style={{display: loadImages.includes(car.id) ? "block" : "none"}}
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">Ano {car.year} | {car.year} km</span>
+                <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+              </div>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
+              <div className="px-2 pb-2">
+                <span className="text-black">
+                  {car.city}
+                </span>
+              </div>
+            </section>
+          </Link>
+        ))}
 
       </main>
     </Container>
